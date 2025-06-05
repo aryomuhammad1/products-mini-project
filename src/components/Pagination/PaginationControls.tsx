0b47833890ac34593d6bf9d522c3817fb80130bf
Aryo <aryomuhammadputra@gmail.com>
2025-06-05 18:38:48 +0700

    feature: add pagination number

--- src/components/Pagination/PaginationControls.tsx
+++ src/components/Pagination/PaginationControls.tsx
@@ -5,6 +5,7 @@ type PaginationControlsProps = {
 };
 
 const PaginationControls = ({ page, setPage, totalPages }: PaginationControlsProps) => {
+    const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
     return (
         <div className="flex justify-center mt-6 space-x-2">
             <button
@@ -13,7 +14,17 @@ const PaginationControls = ({ page, setPage, totalPages }: PaginationControlsPro
                 className="px-3 py-1 bg-gray-200 rounded cursor-pointer disabled:cursor-auto disabled:opacity-50">
                 Prev
             </button>
-            <span className="px-3 py-1">{page}</span>
+            <div className="flex gap-4 px-3 py-1">
+                {pageNumbers.map((n) => {
+                    return (
+                        <p
+                            {...(page !== n ? { className: 'cursor-pointer' } : { className: 'text-gray-500' })}
+                            onClick={() => setPage(n)}>
+                            {n}
+                        </p>
+                    );
+                })}
+            </div>
             <button
                 onClick={() => setPage(Math.min(page + 1, totalPages))}
                 disabled={page === totalPages}
